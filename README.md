093c7d82876f811589bf2bd9cd264172e210515b
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-06-27 11:09:59 -0400

    Add menu page with styles

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # odin-restaurant-page
 
 Photo by Alem Sánchez: https://www.pexels.com/photo/white-receipt-on-table-612790/
+Photo by Kevin  Early: https://www.pexels.com/photo/delicious-thai-basil-chicken-dish-on-plate-37103257/
--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,37 @@
+import tofuBasil from "./tofu-basil.jpg";
+
 export function createMenu() {
     console.log("Opening Contact");
     let main = document.querySelector("#content");
     // create content
     main.innerHTML = "";
-    let text = "Menu page under construction";
-    main.textContent = text;
-}
+    let h1 = document.createElement("h1");
+    h1.textContent = "Menu of the Day";
+    let h2 = document.createElement("h2");
+    h2.textContent = "Thai Basil Tofu";
+    let h3 = document.createElement("h3");
+    h3.textContent = "Dish Description:";
+    let p = document.createElement("p");
+    p.textContent = "Stir-fried organic tofu, garlic, ginger, julienne red pepper, thai basil, dark soy sauce, and chili sauce. Side of Tom Yum fried noodles and vegetable stir fry.";
+    let img = document.createElement('img');
+    img.setAttribute("src", tofuBasil);
+    img.setAttribute("alt", "thai basil tofu");
+    img.setAttribute("width", "320");
+    img.setAttribute("height", "426");
+    let div = document.createElement("div");
+    div.classList.add("Menu");
+    let divLeft = document.createElement("div");
+    let divRight = document.createElement("div");
+    div.classList.add("menu");
+    let imgContainer = document.createElement("div");
+    imgContainer.appendChild(img);
+    divLeft.appendChild(imgContainer);
+    divRight.appendChild(h2);
+    divRight.appendChild(h3);
+    divRight.appendChild(p);
+    main.appendChild(h1);
+    div.appendChild(divLeft);
+    div.appendChild(divRight);
+    main.append(div);
 
-function createMenuItem(nameOfDish, picture, description) {
-    let img = document.createElement("img");
-    img.setAttribute("src", restaurantImage);
-    img.setAttribute("alt", "Empty, clean and neat dishes and glasses in a restaurant");
-    img.setAttribute("width", "720");
-    img.setAttribute("height", "478");
 }
